465280eee398e31b201e8d879cc64ce26628bd70
gabocordova07 <gabocordova07@gmail.com>
2022-03-11 12:40:52 -0600

    Created ChangeScene fucntion

--- pas_game/Assets/Scripts/UiController.cs
+++ pas_game/Assets/Scripts/UiController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UiController : MonoBehaviour
 {
     public Button playButton;
     public Button optionsButton;
-    
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
     void Start()
     {
         
