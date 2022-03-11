4275ff0c6ecda9d2ed1e8a22acd32426dedb5e5a
gabocordova07 <gabocordova07@gmail.com>
2022-03-11 11:59:51 -0600

    Created new MainMenu Scene and added main scripts

---
+++ pas_game/Assets/Scripts/UiController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+    public Button playButton;
+    public Button optionsButton;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
