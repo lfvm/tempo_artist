a00747586086d17cbad53b8dc32741a22ff0817d
gabocordova07 <gabocordova07@gmail.com>
2022-03-18 12:23:05 -0600

    Refactoring and cleanup

--- pas_game/Assets/Scripts/GameController.cs
+++ pas_game/Assets/Scripts/GameController.cs
@@ -19,9 +19,17 @@ public class GameController : MonoBehaviour
     public float dspSongTime;
     public AudioSource musicSource;
 
+<<<<<<< Updated upstream
     void Start()
-    {
+=======
+    //Ref al objeto de notas 
+    public GameObject blueNote;
+    //Lista de Notas
+    public List<GameObject> notes;
 
+    private void Start()
+>>>>>>> Stashed changes
+    {
         //Establecer el texto del score como 0
         uiController.score.text = "Score: 0";
         uiController.combo.text = "0";
@@ -33,14 +41,37 @@ public class GameController : MonoBehaviour
         dspSongTime = (float)AudioSettings.dspTime;
         
         // musicSource.play();
+<<<<<<< Updated upstream
+=======
+        
+        // Crear Noytas
+        for(var i = 0; i<21; i++)
+        {
+            //Crear una nueva nota con la funcion instantiate
+            //Junto con las coordenadas donde vamos a colocar ese objeto
+            //y la rotacion del objeto que se define con Quaternion
+            //Y agregar la columna a la lista de columnas
+            notes.Add( Instantiate( blueNote, new Vector2( -2.428089f, 5 + (i * 2) ), Quaternion.identity ) );
+        }
+>>>>>>> Stashed changes
     }
 
-    void Update()
+    private void Update()
     {
         // Cuantos segundos han pasado desde que comenzo la cancion
         songPosition = (float)(AudioSettings.dspTime - dspSongTime);
 
         // Cuantos beats han pasado desde que comenzo la cancion;
         songPositionInBeats = songPosition / secPerBeat;
+<<<<<<< Updated upstream
+=======
+        
+        //Mover las notas en y
+        foreach (var note in notes)
+        {
+            //Mueve cada nota hacia la abajo en una unidad cada update
+            note.transform.position = note.transform.position + new Vector3(0,-1,0)  * Time.deltaTime * 2;   
+        }
+>>>>>>> Stashed changes
     }
 }
