39360c039f85ff50a22cffb2d73b096ca1422ea0
gabocordova07 <gabocordova07@gmail.com>
2022-03-22 11:02:53 -0600

    Notes now appear to the bpm of the song

--- pas_game/Assets/Scripts/GameController.cs
+++ pas_game/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
@@ -11,6 +12,16 @@ public class GameController : MonoBehaviour
     public GameObject leftHitBox;
     public GameObject rightHitBox;
 
+    public GameObject leftLane;
+    public GameObject rightLane;
+    
+    public GameObject noteHolder;
+
+    public List<GameObject> lanes;
+    public List<GameObject> notes;
+
+    public GameObject NoteObject;
+
     public float songBpm;
     public float secPerBeat;
     public float songPosition;
@@ -18,6 +29,10 @@ public class GameController : MonoBehaviour
     // Cuantos segundos han pasado desde que comenzo la cancion.
     public float dspSongTime;
     public AudioSource musicSource;
+
+    private float lastTime = 0f;
+    private float deltaTime = 0f;
+    private float timer = 0f;
 
     private bool paused;
 
@@ -27,11 +42,14 @@ public class GameController : MonoBehaviour
         uiController.score.text = "Score: 0";
         uiController.combo.text = "0";
 
-        musicSource = GetComponent<AudioSource>();
+        //musicSource = GetComponent<AudioSource>();
 
         secPerBeat = 60f / songBpm;
         
         dspSongTime = (float)AudioSettings.dspTime;
+        
+        lanes.Add(rightLane);
+        lanes.Add(leftLane);
         
         // musicSource.play();
 
@@ -53,6 +71,21 @@ public class GameController : MonoBehaviour
 
         // Cuantos beats han pasado desde que comenzo la cancion;
         songPositionInBeats = songPosition / secPerBeat;
+
+        int rand = Random.Range(0, 2);
+        deltaTime = musicSource.time - lastTime;
+        timer += deltaTime;
+
+        if (timer >= secPerBeat)
+        {
+            // Creacion de las notas
+            GameObject note = Instantiate(NoteObject, new Vector2(lanes[rand].transform.position.x, 7f), Quaternion.identity);
+            notes.Add(note);
+            note.transform.parent = noteHolder.transform;
+            timer -= secPerBeat;
+        }
+
+        lastTime = musicSource.time;
 
         // //Mover las notas en y
         // foreach (var note in notes)
