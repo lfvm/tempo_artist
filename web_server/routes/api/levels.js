a715543d578311cf8b985b4b7efaa3701661ce8c
lfvm <lfvm200@gmail.com>
2022-04-18 18:22:47 -0500

    Se creo archivo para manejar las rutas de los niveles

---
+++ web_server/routes/api/levels.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { check } = require('express-validator');
+let router = express.Router();
+
+//Archivo para manejar las rutas de los niveles
+
+
+
+
+
+module.exports = router;
