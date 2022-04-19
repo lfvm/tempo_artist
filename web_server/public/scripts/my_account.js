edbf4e396a917fea68b22c3c0dfd1f939ff02c0a
lfvm <lfvm200@gmail.com>
2022-04-19 11:57:23 -0500

    Se agrega funcionalidad para cargar los datos del usuario

--- web_server/public/scripts/my_account.js
+++ web_server/public/scripts/my_account.js
@@ -13,3 +13,19 @@ const logOut = (e) => {
     window.location.href = '/login';
 
 }
+
+
+const setInputValues = () => {
+
+    const mail = localStorage.getItem('mail');
+    const name = localStorage.getItem('nombre');
+    const lastName = localStorage.getItem('apellidos');
+
+    document.getElementById('title').innerHTML = name + " " + lastName;
+    document.getElementById('name_input').value = name;
+    document.getElementById('last_input').value = lastName;
+    document.getElementById('email_input').value = mail
+
+
+
+}
