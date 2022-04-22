390a6860304e03fd65b8480544617393aefa4ec0
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-04-22 10:22:41 -0500

    Animacion del texto implementada

--- web_server/public/scripts/check_auth.js
+++ web_server/public/scripts/check_auth.js
@@ -1,12 +1,12 @@
-const checkAuth = (e) => {
-    //Si se encuentran los datos de un usuario autenticado quedarse en home, de lo contrario
-    //ir a login
-    e.preventDefault();
-    const mail = localStorage.getItem('mail') ?? '';
+// const checkAuth = (e) => {
+//     //Si se encuentran los datos de un usuario autenticado quedarse en home, de lo contrario
+//     //ir a login
+//     e.preventDefault();
+//     const mail = localStorage.getItem('mail') ?? '';
     
-    if(mail == ''){
-        location.href = '/login';
+//     if(mail == ''){
+//         location.href = '/login';
 
-    }
+//     }
 
-}
+// }
