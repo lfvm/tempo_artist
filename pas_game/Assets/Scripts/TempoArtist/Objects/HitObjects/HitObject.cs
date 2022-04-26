828ecfba14c16636ba3fbbd2e8b594fbd0d2e9c1
gabocordova07 <gabocordova07@gmail.com>
2022-04-26 14:51:03 -0500

    HitObject base class

--- pas_game/Assets/Scripts/TempoArtist/Objects/HitObjects/HitObject.cs
+++ pas_game/Assets/Scripts/TempoArtist/Objects/HitObjects/HitObject.cs
@@ -1,6 +1,6 @@
 ﻿namespace TempoArtist.Objects.HitObjects
 {
-    public abstract class HitObject
+    public class HitObject
     {
         public int X { get; set; }
         public int Y { get; set; }
